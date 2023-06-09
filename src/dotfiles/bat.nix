048f7be1b816666d23f3a256203dad5939cda57d
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2023-06-09 08:58:18 -0300

    Add bat config

---
+++ src/dotfiles/bat.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }: {
+  enable = true;
+
+  # Set bat config
+  config = { theme = "Catppuccin-mocha"; };
+  # Add themes
+  themes = {
+    Catppuccin-mocha = builtins.readFile (pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "bat";
+      rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+      sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+    } + "/Catppuccin-mocha.tmTheme");
+  };
+}
