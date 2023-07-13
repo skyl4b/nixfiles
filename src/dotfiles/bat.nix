dcd28c77bef5eb63b2959f15e022fc3ef5762aa1
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2023-07-13 01:08:34 -0300

    Remove bat unused comment

--- src/dotfiles/bat.nix
+++ src/dotfiles/bat.nix
@@ -5,14 +5,6 @@
   config = { theme = "Catppuccin-mocha"; };
 
   # Add themes
-  # themes = {
-  #   Catppuccin-mocha = builtins.readFile (pkgs.fetchFromGitHub {
-  #     owner = "catppuccin";
-  #     repo = "bat";
-  #     rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-  #     sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-  #   } + "/Catppuccin-mocha.tmTheme");
-  # };
   themes = {
     Catppuccin-mocha =
       builtins.readFile (extras.bat-theme + "/Catppuccin-mocha.tmTheme");
