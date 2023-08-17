a7e51630488dde5d6ffbfb540c495386443b20ba
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2023-08-17 17:31:53 -0300

    Simplify package importing with default.nix files

--- src/programs.nix
+++ src/programs.nix
@@ -4,21 +4,11 @@ let
   # Dotfiles loader
   dotfiles = builtins.readDir ./dotfiles;
 
-  # Filter to config paths inside directories or ".nix" files
-  configPaths = builtins.mapAttrs (name: type:
-    if type == "directory" then
-      ./. + "/dotfiles/${name}/config.nix"
-    else if inputs.pkgs.lib.hasSuffix ".nix" name then
-      ./. + "/dotfiles/${name}"
-    else
-      null) dotfiles;
-
   # Import each program's configuration into a map
-  configs = builtins.listToAttrs (builtins.map (config: {
-    name = inputs.pkgs.lib.removeSuffix ".nix" (baseNameOf config);
-    value = import configPaths.${config} { inherit inputs; };
-  }) (builtins.filter (config: configPaths.${config} != null)
-    (builtins.attrNames configPaths)));
+  configs = builtins.listToAttrs (builtins.map (program: {
+    name = inputs.pkgs.lib.removeSuffix ".nix" (baseNameOf program);
+    value = import ./dotfiles/${program} { inherit inputs; };
+  }) (builtins.attrNames dotfiles));
 in configs // {
   # Extra configs
 
