a9e7d43dd73fe7bdd4df7fa99b99416604ac7eb0
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2023-10-31 19:45:39 -0300

    overlays: fix copilot-lsp path

--- src/overlays/copilot-lsp.nix
+++ src/overlays/copilot-lsp.nix
@@ -1,7 +1,7 @@
-inputs:
+{ inputs, path, ... }:
 
 final: prev: {
-  copilot-lsp = import ../packages/copilot-lsp.nix {
+  copilot-lsp = import (path + "/../packages/copilot-lsp.nix") {
     pkgs = prev;
     copilot-lsp-src = inputs.copilot-lsp-src;
   };
