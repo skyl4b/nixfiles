77600e3a5725ac158eb0f8fb1c0d1e48ed31a8dc
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2023-10-31 22:00:04 -0300

    git: add many useful configs

--- src/dotfiles/git.nix
+++ src/dotfiles/git.nix
@@ -14,12 +14,65 @@
   # Extra configs
   extraConfig = {
     merge = {
+      # Set mergetool
       tool = "nvimdiff";
       guitool = "nvimdiff";
+      # Clean up backup / temp files on exit
+      keepBackup = false;
+      keepTemporaries = false;
     };
+
     diff = {
+      # Set difftool
       tool = "nvimdiff";
       guitool = "nvimdiff";
+      # Use mnemonic prefixes instead of a/ b/
+      # ie: i - index, w - worktree, c - commit, o - object
+      mnemonicPrefix = true;
+      # Enable rename detection
+      rename = true;
     };
+
+    color = {
+      # Color output if possible
+      ui = "auto";
+      # Use cyan as color for upstream in git branch -vv
+      upstream = "cyan";
+    };
+
+    # Enable line numbers in grep
+    grep.lineNumber = true;
+
+    log = {
+      # Abbreviate commit sha when possible
+      abbrevCommit = true;
+
+      # Follow history through renames for
+      # single files
+      follow = true;
+    };
+
+    push = {
+      # Push to tracking remote, regardless of branch name
+      # NOTE: prefer using the same name when possible
+      default = "upstream";
+      # Push tags together by default
+      followTags = true;
+      # Setup branch on remote if no tracking branch is set
+      autoSetupRemote = true;
+    };
+
+    # Sort tags version numbers when applicable
+    tag.sort = "version:refname";
+
+    # Define the version sorting strategy
+    versionsort.suffix = [
+      "-pre"
+      ".pre"
+      "-beta"
+      ".beta"
+      "-rc"
+      ".rc"
+    ];
   };
 }
