048f6aed880b9a204c8d2148db0945be24152bf5
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2024-02-01 16:22:57 -0300

    Add desktop environment configuration folder

---
+++ src/systems/desktop_environments/gnome.nix
@@ -0,0 +1,19 @@
+# Configuration of a gnome desktop environment using a few custom extensions
+# such as pop shell
+{ pkgs, ... }: {
+  # Enable the GNOME Desktop Environment
+  services.desktopManager.gnome.enable = true;
+
+  # Gnome useful packages
+  environment.systemPackages = with pkgs; [
+    # Gnome specific tools
+    gnome.gnome-software
+    gnome.gnome-tweaks
+    gnomeExtensions.appindicator
+    gnomeExtensions.dash-to-dock
+
+    # Pop shell
+    gnomeExtensions.pop-shell
+    pop-launcher
+  ];
+}
