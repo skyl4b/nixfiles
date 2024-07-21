cd3f699d8e80451062805af81f34ec174ab1ebd4
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2024-07-21 11:26:42 -0300

    yazi: edit on e

--- src/dotfiles/yazi/default.nix
+++ src/dotfiles/yazi/default.nix
@@ -7,12 +7,20 @@ _: {
   plugins.smart-enter = ./smart-enter;
 
   # Keymap
-  # Enable smart open on 'l'
-  keymap.manager.prepend_keymap = [{
-    on = [ "l" ];
-    run = "plugin --sync smart-enter";
-    desc = "Enter the child directory, or open the file";
-  }];
+  keymap.manager.prepend_keymap = [
+    # Enable smart open on 'l'
+    {
+      on = [ "l" ];
+      run = "plugin --sync smart-enter";
+      desc = "Enter the child directory, or open the file";
+    }
+    # Edit on "e"
+    {
+      on = [ "e" ];
+      run = "shell '$EDITOR \"$@\"' --confirm --block";
+      desc = "Open with editor";
+    }
+  ];
 
   # Theme
   catppuccin.enable = true;
